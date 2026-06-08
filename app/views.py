94677c9eb33d23cea92205b3a32e568289d8042c
Kaksya <sameertamang34w@gmail.com>
2026-06-08 16:07:46 +0545

    changed to viewsets from view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from typing import cast
-from django.shortcuts import render
-from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
@@ -12,66 +12,41 @@ from django.db.models import Count, Q
 
 # Create your views here.
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def register_user(request):
-    serializer = RegisterSerializer(data=request.data)
+class RegisterViewSet(viewsets.ModelViewSet):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(
-         {'message': 'User registered successfully'},
-         status=status.HTTP_201_CREATED
-      )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "message": "User registered successfully"},
+                    status=status.HTTP_201_CREATED
+            )
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET"])
-@permission_classes([AllowAny])
-def get_blogs(request):
-    blogs = Blog.objects.filter(is_published=True)
-    serializer = BlogSerializer(blogs, many=True)
-    return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET"])
-@permission_classes([AllowAny])
-def get_blog_detail(request, blog_id):
+class BlogViewSet(viewsets.ModelViewSet):
+    serializer_class = BlogSerializer
 
-    try:
-        blog = Blog.objects.get(id=blog_id)
-    except Blog.DoesNotExist:
-        return Response({"error": "Blog not found"}, status=status.HTTP_404_NOT_FOUND)
+    def get_permissions(self):
+        if self.action in ["list", "retrieve", "related"]:
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
-    tag_ids = blog.tags.values_list("id", flat=True)
+    def get_queryset(self):
+        if self.action in ["list", "retrieve", "related"]:
+            return Blog.objects.filter(is_published=True).order_by("-created_at")
 
-    related_blogs = (
-        Blog.objects.filter(category=blog.category, is_published=True)
-        .exclude(id=blog.pk)
-        .annotate(matching_tags=Count("tags", filter=Q(tags__in=tag_ids)))
-        .order_by("-matching_tags", "-created_at")[:5]
-    )
+        return Blog.objects.all().order_by("-created_at")
 
-    blog_serializer = BlogSerializer(blog)
-    related_serializer = BlogSerializer(related_blogs, many=True)
-
-    return Response(
-        {"blog": blog_serializer.data, "related_blogs": related_serializer.data}
-    )
-
-
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def create_blog(request):
-    serializer = BlogSerializer(data=request.data)
-
-    if serializer.is_valid():
-        blog = cast(Blog, serializer.save(user=request.user))
+    def perform_create(self, serializer):
+        blog = cast(Blog, serializer.save(user=self.request.user))
 
         tag_names = generate_tags_for_blog(title=blog.title, content=blog.content)
-
-        print("GENERATED TAGS:", tag_names)
 
         for tag_name in tag_names:
             tag_name = tag_name.strip().lower()
@@ -80,124 +55,127 @@ def create_blog(request):
                 tag, created = Tag.objects.get_or_create(name=tag_name)
                 blog.tags.add(tag)
 
-        response_serializer = BlogSerializer(blog)
+    def update(self, request, *args, **kwargs):
+        blog = self.get_object()
 
-        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+        if blog.user != request.user:
+            return Response(
+                {"error": "You can only update your own blog"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        partial = kwargs.pop("partial", True)
+        serializer = self.get_serializer(blog, data=request.data, partial=partial)
 
+        if serializer.is_valid():
+            blog = cast(Blog, serializer.save())
 
-@api_view(["PUT"])
-@permission_classes([IsAuthenticated])
-def update_blog(request, blog_id):
-    try:
-        blog = Blog.objects.get(id=blog_id)
-    except Blog.DoesNotExist:
-        return Response({"error": "Blog not found"}, status=status.HTTP_404_NOT_FOUND)
+            tag_names = generate_tags_for_blog(title=blog.title, content=blog.content)
 
-    if blog.user != request.user:
-        return Response(
-            {"error": "You can only update your own blog"},
-            status=status.HTTP_403_FORBIDDEN,
+            blog.tags.clear()
+
+            for tag_name in tag_names:
+                tag_name = tag_name.strip().lower()
+
+                if tag_name:
+                    tag, created = Tag.objects.get_or_create(name=tag_name)
+                    blog.tags.add(tag)
+
+            response_serializer = self.get_serializer(blog)
+            return Response(response_serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        blog = self.get_object()
+
+        if blog.user != request.user:
+            return Response(
+                {"error": "You can only delete your own blog"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        blog.delete()
+        return Response({"message": "Blog deleted successfully"})
+
+    def retrieve(self, request, *args, **kwargs):
+        blog = self.get_object()
+
+        tag_ids = blog.tags.values_list("id", flat=True)
+
+        related_blogs = (
+            Blog.objects.filter(category=blog.category, is_published=True)
+            .exclude(id=blog.pk)
+            .annotate(matching_tags=Count("tags", filter=Q(tags__in=tag_ids)))
+            .order_by("-matching_tags", "-created_at")[:5]
         )
 
-    serializer = BlogSerializer(blog, data=request.data, partial=True)
+        blog_serializer = self.get_serializer(blog)
+        related_serializer = self.get_serializer(related_blogs, many=True)
 
-    if serializer.is_valid():
-        blog = cast(Blog, serializer.save())
-
-        tag_names = generate_tags_for_blog(title=blog.title, content=blog.content)
-
-        blog.tags.clear()
-
-        for tag_name in tag_names:
-            tag_name = tag_name.strip().lower()
-
-            if tag_name:
-                tag, created = Tag.objects.get_or_create(name=tag_name)
-                blog.tags.add(tag)
-
-        response_serializer = BlogSerializer(blog)
-
-        return Response(response_serializer.data)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["DELETE"])
-@permission_classes([IsAuthenticated])
-def delete_blog(request, blog_id):
-    try:
-        blog = Blog.objects.get(id=blog_id)
-    except Blog.DoesNotExist:
-        return Response({"error": "Blog not found"}, status=status.HTTP_404_NOT_FOUND)
-
-    if blog.user != request.user:
         return Response(
-            {"error": "You can only delete your own blog"},
-            status=status.HTTP_403_FORBIDDEN,
+            {
+                "blog": blog_serializer.data,
+                "related_blogs": related_serializer.data,
+            }
         )
 
-    blog.delete()
-    return Response({"message": "Blog deleted successfully"})
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_permissions(self):
+        if self.action in ["list"]:
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        blog_id = self.kwargs.get("blog_pk")
+
+        if blog_id:
+            return Comment.objects.filter(blog_id=blog_id).order_by("-id")
+
+        return Comment.objects.all().order_by("-id")
+
+    def create(self, request, *args, **kwargs):
+        blog_id = self.kwargs.get("blog_pk")
+
+        try:
+            blog = Blog.objects.get(id=blog_id, is_published=True)
+        except Blog.DoesNotExist:
+            return Response(
+                {"error": "Blog not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user, blog=blog)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        comment = self.get_object()
+
+        if comment.user != request.user:
+            return Response(
+                {"error": "You can only delete your own comment"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        comment.delete()
+        return Response({"message": "Comment deleted successfully"})
 
 
-@api_view(["GET"])
-@permission_classes([AllowAny])
-def get_comments(request, blog_id):
-    comments = Comment.objects.filter(blog_id=blog_id)
-    serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
 
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def create_comment(request, blog_id):
-    try:
-        blog = Blog.objects.get(id=blog_id, is_published=True)
-    except Blog.DoesNotExist:
-        return Response({"error": "Blog not found"}, status=status.HTTP_404_NOT_FOUND)
-
-    serializer = CommentSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save(user=request.user, blog=blog)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["DELETE"])
-@permission_classes([IsAuthenticated])
-def delete_comment(request, comment_id):
-    try:
-        comment = Comment.objects.get(id=comment_id)
-    except Comment.DoesNotExist:
-        return Response(
-            {"error": "Comment not found"}, status=status.HTTP_404_NOT_FOUND
-        )
-
-    if comment.user != request.user:
-        return Response(
-            {"error": "You can only delete your own comment"},
-            status=status.HTTP_403_FORBIDDEN,
-        )
-
-    comment.delete()
-    return Response({"message": "Comment deleted successfully"})
-
-
-@api_view(["GET"])
-@permission_classes([AllowAny])
-def get_categories(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-@permission_classes([AllowAny])
-def get_tags(request):
-    tags = Tag.objects.all()
-    serializer = TagSerializer(tags, many=True)
-    return Response(serializer.data)
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [AllowAny]
